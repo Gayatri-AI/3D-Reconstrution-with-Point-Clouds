456272573484b4df194f0c9081b16460ef224da1
Gayatri-AI <79926769+Gayatri-AI@users.noreply.github.com>
2022-06-05 00:55:40 +0530

    Add files via upload

---
+++ Depth Maps/main.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+left_image = cv.imread('tsukuba_l.png', cv.IMREAD_GRAYSCALE)
+right_image = cv.imread('tsukuba_r.png', cv.IMREAD_GRAYSCALE)
+
+stereo = cv.StereoBM_create(numDisparities=0, blockSize=21)
+# For each pixel algorithm will find the best disparity from 0
+# Larger block size implies smoother, though less accurate disparity map
+depth = stereo.compute(left_image, right_image)
+
+print(depth)
+
+cv.imshow("Left", left_image)
+cv.imshow("right", right_image)
+
+plt.imshow(depth)
+plt.axis('off')
+plt.show()
